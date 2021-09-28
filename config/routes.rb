4a94bfc8951b641372b574c587003fe9f8771746
mwaqas6665 <waqas.akbar@devsinc.com>
2021-09-28 17:01:21 +0500

    Mail for account activation

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   root 'static_pages#home'
   resources :users
+  resources :account_activations, only: [:edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
